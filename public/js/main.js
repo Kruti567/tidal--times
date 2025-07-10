d86658850471b653f0ed4bbadfb9ad0c82c747f9
Kruti Mehta <mehtakruti98@gmail.com>
2025-07-10 16:56:43 +0530

    footer

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Load header
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        });
+
+    // Load footer
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-placeholder").innerHTML = data;
+        });
+});
+
 let slideIndex = 0;
 showSlides();
 
